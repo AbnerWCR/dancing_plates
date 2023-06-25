1ebf39a0613b1f3a9c66b42b757a50577c45e3cf
AbnerWCR <abnerwcrodrigues@gmail.com>
2023-06-25 11:50:02 -0300

    feat: adicionando final screen

--- KIT_DEV_ALLEGRO/main.c
+++ KIT_DEV_ALLEGRO/main.c
@@ -260,7 +260,54 @@ void draw_score(ALLEGRO_FONT *font, Jogador jogador){
 	free(text);
 }
 
-void set_record(Jogador jogador){
+void draw_final_screen(ALLEGRO_FONT *font, Jogador jogador, int is_recorde){
+	char *score = (char*)malloc(10001*sizeof(char));
+	sprintf(score, "%.3f", jogador.score);
+	char *text = (char*)malloc(50*sizeof(char));
+
+	if(is_recorde == 1){			
+		strcpy(text, "Novo recorde: ");
+		strcat(text, score);
+	}
+	else{
+		strcpy(text, "Score final: ");
+		strcat(text, score);
+	}
+	al_draw_filled_rectangle(SCREEN_W/2 - 150, SCREEN_H/2 - 60,
+							SCREEN_W/2 + 150, SCREEN_H/2 + 60,
+							al_map_rgb(0, 0, 0));
+	al_draw_text(font,
+				al_map_rgb(255, 255, 255), SCREEN_W/2 - 100, SCREEN_H/2 - 30, ALLEGRO_ALIGN_LEFT,
+				text);
+
+	al_draw_filled_rectangle(SCREEN_W/2 - 145, SCREEN_H/2 + 20,
+							SCREEN_W/2 - 5, SCREEN_H/2 + 45,
+							al_map_rgb(255, 255, 255));
+	al_draw_text(font,
+				al_map_rgb(0, 0, 0), SCREEN_W/2 - 140, SCREEN_H/2 + 25, ALLEGRO_ALIGN_LEFT,
+				"Sair");
+
+	al_draw_filled_rectangle(SCREEN_W/2+5, SCREEN_H/2 + 20,
+							SCREEN_W/2 + 140, SCREEN_H/2 + 45,
+							al_map_rgb(255, 255, 255));
+	al_draw_text(font,
+				al_map_rgb(0, 0, 0), SCREEN_W/2 + 10, SCREEN_H/2 + 25, ALLEGRO_ALIGN_LEFT,
+				"Jogar novamente");
+	free(text);
+	free(score);
+}
+
+int decisao(int x, int y){
+	if(!(y >= SCREEN_H/2 + 20 && y <= SCREEN_H/2 + 45))
+		return 2;
+	if(x >= SCREEN_W/2 - 145 && x <= SCREEN_W/2 - 5)
+		return 0;
+	if(x >= SCREEN_W/2+5 && x <= SCREEN_W/2 + 140)
+		return 1;
+	return 2;
+}
+
+int set_record(Jogador jogador){
 	FILE *fp;
 	fp = fopen("recorde_jogador.txt", "a+");
 	if(fp == NULL){
@@ -280,14 +327,18 @@ void set_record(Jogador jogador){
 	}
 	printf("\nrecorde: %.3f", record);
 
-	if(jogador.score < record)
-		return;
+	if(jogador.score < record){
+		fclose(fp);
+		return 0;
+	}
 
 	char *score = (char*)malloc(10001*sizeof(char));
 	sprintf(score, "%.3f\n", jogador.score);
 	fputs(score, fp);
 	
+	free(score);
 	fclose(fp);
+	return 1;
 }
  
 int main(int argc, char **argv){
@@ -334,6 +385,11 @@ int main(int argc, char **argv){
 		fprintf(stderr, "failed to install keyboard!\n");
 		return -1;
 	}
+	//instala o teclado
+	if(!al_install_mouse()) {
+		fprintf(stderr, "failed to install mouse!\n");
+		return -1;
+	}
 	
 	//inicializa o modulo allegro que carrega as fontes
 	al_init_font_addon();
@@ -348,7 +404,12 @@ int main(int argc, char **argv){
     ALLEGRO_FONT *size_32 = al_load_font("arial.ttf", 32, 1);   
 	if(size_32 == NULL) {
 		fprintf(stderr, "font file does not exist or cannot be accessed!\n");
-	}	
+	}
+
+	ALLEGRO_FONT *size_14 = al_load_font("arial.ttf", 14, 1);   
+	if(size_14 == NULL) {
+		fprintf(stderr, "font file does not exist or cannot be accessed!\n");
+	}		
 
 	ALLEGRO_FONT *size_12 = al_load_font("arial.ttf", 12, 1);   
 	if(size_12 == NULL) {
@@ -370,7 +431,7 @@ int main(int argc, char **argv){
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	//registra na fila os eventos de teclado (ex: pressionar uma tecla)
 	al_register_event_source(event_queue, al_get_keyboard_event_source()); 	
-	
+	al_register_event_source(event_queue, al_get_mouse_event_source());
 	
 	//JOGADOR
 	Jogador jogador;
@@ -379,20 +440,35 @@ int main(int argc, char **argv){
 	//PRATOS
 	Prato *pratos = (Prato*)malloc(NUM_PRATOS*sizeof(Prato));
 	inicializaPratos(pratos);
-	
+
 	//inicia o temporizador
 	al_start_timer(timer);	
-	
+
+	int end_game = 1;
 	int i;
-	int playing=1;
-	while(playing) {
-		
+	int playing = 1;
+	while(playing) {		
 		ALLEGRO_EVENT ev;
 		//espera por um evento e o armazena na variavel de evento ev
 		al_wait_for_event(event_queue, &ev);
 		
 		//se o tipo de evento for um evento do temporizador, ou seja, se o tempo passou de t para t+1
-		if(ev.type == ALLEGRO_EVENT_TIMER) {					
+		if(ev.type == ALLEGRO_EVENT_TIMER && end_game != 2) {
+			printf("\nCaiu event timer");
+			if(end_game == 0){
+				printf("\nCaiu no end_game 0");
+				free(pratos);
+				al_stop_timer(timer);
+
+				int is_recorde = 0;
+				is_recorde = set_record(jogador);
+				draw_final_screen(size_14, jogador, is_recorde);
+				al_flip_display();
+				
+				continue;
+			}		
+
+			printf("\nInicio jogo");
 			int status = 0;
 			status = check_plates(pratos);
 			if (status == 0)
@@ -417,10 +493,13 @@ int main(int argc, char **argv){
 		}
 		//se o tipo de evento for o fechamento da tela (clique no x da janela)
 		else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-			playing = 0;
+			if (end_game == 0)
+				playing = 0;
+			else 
+				end_game = 0;
 		}		
 		//se o tipo de evento for um pressionar de uma tecla
-		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN && end_game != 2) {
 			switch (ev.keyboard.keycode){
 				case ALLEGRO_KEY_A:
 					jogador.mov_esq = 1;
@@ -435,7 +514,7 @@ int main(int argc, char **argv){
 			}			
 		}
 		//se o tipo de evento for um pressionar de uma tecla
-		else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
+		else if(ev.type == ALLEGRO_EVENT_KEY_UP && end_game != 2) {
 			switch (ev.keyboard.keycode){
 				case ALLEGRO_KEY_A:
 					jogador.mov_esq = 0;
@@ -444,17 +523,49 @@ int main(int argc, char **argv){
 					jogador.mov_dir = 0;
 					break;
 			}			
-		}		
-		
-		
-	}
+		}	
+		// click mouse
+		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && end_game != 1){
+			printf("\nmouse down on: x: %d, y: %d", ev.mouse.x, ev.mouse.y);
+			int j_decisao = decisao(ev.mouse.x, ev.mouse.y);
+			printf("\nDecisao: %d", j_decisao);
+
+			if(j_decisao == 2){
+				end_game = j_decisao;
+				continue;
+			}
+			else if(j_decisao == 1){
+				playing = j_decisao;
+				end_game = j_decisao;
+
+				InicializaJogador(&jogador);
 	
-	set_record(jogador);
-	free(pratos);
+				//PRATOS
+				pratos = (Prato*)malloc(NUM_PRATOS*sizeof(Prato));
+				inicializaPratos(pratos);
+
+				al_destroy_timer(timer);
+				timer = al_create_timer(1.0 / FPS);
+				if(!timer) {
+					fprintf(stderr, "failed to create timer!\n");
+					return -1;
+				}
+
+				al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+				//inicia o temporizador
+				al_start_timer(timer);
+				printf("\nJogar novamente, sucesso");	
+			}
+			else{
+				playing = 0;
+			}
+		}
+	}
+
 	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
-	
  
 	return 0;
 }
